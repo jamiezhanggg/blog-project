6b382d0066f0e25ac5f8be0b59917e0a97031736
JamieZhang <u7720838@anu.edu.au>
2024-10-13 19:34:40 +1100

    md5 encrypt

---
+++ backend/src/utils/md5.js
@@ -0,0 +1,18 @@
+const md5 = require('md5');
+const salt = "SALT";
+
+const md5Password = (password) => {
+    return new Promise((resolve, reject) => {
+        const md5Pwd = md5(password + salt);
+        resolve(md5Pwd);
+    })
+}
+
+const matchPassword = (savedPassword, usrPassword) => {
+    return new Promise((resolve, reject) => {
+        const md5Pwd = md5(usrPassword + salt);
+        let match = md5Pwd == savedPassword ? true:false;
+        resolve(match);
+    })
+}
+module.exports = {md5Password, matchPassword}
