cfefdecd50882fab723291b3edf715062da6e4e9
JamieZhang <u7720838@anu.edu.au>
2024-10-17 10:54:18 +1100

    login complete

--- backend/src/controller/user.js
+++ backend/src/controller/user.js
@@ -1,18 +1,10 @@
 const HttpException = require("../exceptions/http.exceptions");
-const { validateCreateUser } = require("../utils/validate/user.validate");
+const { validateCreateUser, validateUserLogin } = require("../utils/validate/user.validate");
 const User = require('../model/user');
-const { md5Password } = require("../utils/md5");
+const { md5Password, matchPassword } = require("../utils/md5");
+const { sign, decode } = require('../utils/jwt');
 
 module.exports.createUser = async (req, res) => {
-    // res.json({
-    //     status: 200,
-    //     message: "success",
-    //     data: {
-    //         code:1,
-    //         message: "user succssfully added!",
-    //         data:{}
-    //     }
-    // })
     try {
         let { username, password, email } = req.body.user;
         let { error, validate } = validateCreateUser(username, password, email);
@@ -31,12 +23,11 @@ module.exports.createUser = async (req, res) => {
             let data = {};
             data.username = username;
             data.email = email;
-            data.token = null;
-            // TODO: implement token logic
+            data.token = await sign(username, email);
             data.avatar = null;
             data.bio = null;
             res.json({
-                status: 200,
+                status: 201,
                 message: "success",
                 data: {
                     code: 1,
@@ -46,8 +37,38 @@ module.exports.createUser = async (req, res) => {
             })
         }
     } catch (error) {
+        next(error);
+    }
+}
 
+module.exports.login = async (req, res) => {
+    try{
 
+        let {email, password} = req.body.user;
+        let {error, validate} = user.validateUserLogin(email, password);
+        if(!validate)
+            throw new HttpException(400, 'validation error', error);
+        let user = await User.findByPk(email);
+        if(!user) {
+            throw new HttpException(401, 'user donnot exist', 'user donnot exist');
+        }
+        const encryptedPassword = await md5Password(password);
+        const match = await matchPassword(encryptedPassword, password);
+        if(!match) {
+            throw new HttpException(401, 'wrong password', 'wrong password');
+        }
+        delete user.dataValues.password;
+        user.dataValues.token = await sign(
+            user.dataValues.username,
+            user.dataValues.password
+        );
+        return res.status(200).json({
+            status: 1,
+            data: user, 
+            message: 'login succeed'
+        })
+    } catch (err) {
+        next(err);
     }
 }
 
--- backend/src/utils/validate/user.validate.js
+++ backend/src/utils/validate/user.validate.js
@@ -17,3 +17,15 @@ module.exports.validateCreateUser = (username, password, email) => {
     let validate = Object.keys(error).length<1? true:false;
     return {error, validate};
 }
+
+module.exports.validateUserLogin = (email, password) => {
+    let error = {};
+    if(validator.isEmpty(email))
+        error.email = 'email cannot be empty';
+    if(validator.isEmpty(password))
+        error.password = 'password cannot be empty';
+    if(!validator.isEmpty(email) && !validator.isEmail(email))
+        error.email = 'email not legal';
+    let validate = Object.keys(error).length==0 ? true:false;
+    return { error, validate };
+}
